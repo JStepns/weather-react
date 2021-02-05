5a7a623679aae9483fb3ccf93fea73c457bb3832
Jacqueline Stephens <jacqui.stephens3@gmail.com>
2021-02-05 12:00:14 +1000

    Added author line

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Ipswich</h1>
+      <footer>This project was coded by Jacqueline Stephens and is open-sourced on <a href = "https://github.com/JStepns/weather-react" target = "_blank" rel = "noreferrer">GitHub</a> and hosted on Netlify.</footer>
     </div>
   );
 }
