82e97c108218556257fa2240b2d30ce26b94ed61
Jacqueline Stephens <jacqui.stephens3@gmail.com>
2021-02-05 12:47:21 +1000

    Added bootstrap & jquery

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className = "container">
       <h1>Ipswich</h1>
       <footer>This project was coded by Jacqueline Stephens and is open-sourced on <a href = "https://github.com/JStepns/weather-react" target = "_blank" rel = "noreferrer">GitHub</a> and hosted on Netlify.</footer>
+      </div>
     </div>
   );
 }
